a723aa1a4e9a7e8b00b9dc17f5909ab882d14e31
loxyg <80475765+loxyg@users.noreply.github.com>
2021-05-28 19:27:13 +0200

    footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 
@@ -8,9 +9,10 @@ function App() {
     <div className="App">
       <Router>
         <Header/>
+        <Footer/>
       </Router>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import './Footer.css'
+function Footer(){
+    return(
+        <div className="footerarea">
+            <div className="footerarea_top">
+                <a href="#" ><p>Back to top</p></a>
+            </div>
+
+            <div className="footerarea_links">
+                <div className="footerarea_linkarea">
+                        <span> 
+                            <h2>Title</h2>
+                            <p>content</p>
+                            <p>content</p>
+                            <p>content</p>
+                        </span>
+                </div>
+
+                <div className="footerarea_linkarea">
+                        <span> 
+                            <h2>Title</h2>
+                            <p>content</p>
+                            <p>content</p>
+                            <p>content</p>
+                        </span>
+                </div>
+
+                <div className="footerarea_linkarea">
+                        <span> 
+                            <h2>Title</h2>
+                            <p>content</p>
+                            <p>content</p>
+                            <p>content</p>
+                        </span>
+                </div>
+
+                <div className="footerarea_linkarea">
+                        <span> 
+                            <h2>Title</h2>
+                            <p>content</p>
+                            <p>content</p>
+                            <p>content</p>
+                        </span>
+                </div>
+           </div>
+    </div>
+    )
+}
+
+export default Footer
