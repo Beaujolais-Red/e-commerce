6aa15eb71d25984b73ed3cfbc137d16d7ae62f7e
Beaujolais-Red <a_prunell@etu-webschoolfactory.fr>
2021-05-28 19:38:47 +0200

    fourth

--- src/App.js
+++ src/App.js
@@ -2,16 +2,26 @@ import './App.css';
 import Header from "./components/Header";
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Homepage from "./components/Homepage"
+import Checkout from "./components/Checkout"
 
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Header/>
-        <Homepage/>
-      </Router>
-    </div>
+    <Router>
+      <div className="App">
+          <Switch>
+            <Route path="/">
+              <Header/>
+              <Homepage/>
+            </Route>
+            <Route path="/checkout">
+              <Header/>
+              <Checkout/>
+            </Route>
+          </Switch>
+      </div>
+    </Router>
+
   );
 }
 
